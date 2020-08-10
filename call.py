55c48642c9b825f971280eaefebd559d9009da96
FR13NDS <64352865+FR13NDS666@users.noreply.github.com>
2020-08-10 12:17:18 +0700

    Update call.py

--- call.py
+++ call.py
@@ -4,8 +4,8 @@ def baner():
     print ("""
 \t\033[1;97mSPAM CALL
 \t\033[90m----------
-\033[1;97m[\033[1;94m•\033[1;97m]Creator:\033[1;92mKnifer12
-\033[1;97m[\033[1;94m•\033[1;97m]Github :\033[4;92mgithub.com/BladeKnife\033[00m
+\033[1;97m[\033[1;94m•\033[1;97m]Creator:\033[1;92mFR13NFS
+\033[1;97m[\033[1;94m•\033[1;97m]Github :\033[4;92mgithub.com/FR13NDS666\033[00m
 \033[90m___________________________________""")
 def countdownTimer(start_minute, start_second):
     total_second = start_minute * 60 + start_second
